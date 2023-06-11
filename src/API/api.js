73b9430b750ad768521e6d7b07cf3e14f1505888
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-06-11 13:07:17 +0300

    no

---
+++ src/API/api.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { KEY, BASE_URL } from './api-key';
+
+export async function getDayTrending(page = 1) {
+  const url = `${BASE_URL}/trending/all/day?api_key=${KEY}&language=en-US&page=${page}`;
+  return await axios
+    .get(url)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => console.log(error));
+}
+
+export async function getVideos(movie_id) {
+  const url = `${BASE_URL}/movie/${movie_id}/videos?api_key=${KEY}&language=en-US`;
+  return await axios
+    .get(url)
+    .then(response => {
+      return response.data.results;
+    })
+    .catch(error => {});
+}
+
+// Фетч фильма по его ID
+export async function getMovieById(id) {
+  try {
+    const { data } = await axios.get(`${ID_URL}${id}?api_key=${KEY}`);
+
+    const result = {
+      ...data,
+      year: createYear(data),
+      customGenres: createGenresFromID(data),
+    };
+
+    return result;
+  } catch (error) {
+    console.error('Smth wrong with api ID fetch' + error);
+  }
+}
+
+export const getSecondMovieById = async id => {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}`
+    );
+    const result = {
+      ...data,
+    };
+    return result;
+  } catch (error) {
+    console.error('Smth wrong with api ID fetch' + error);
+  }
+};
+
+export const getMovieById2 = async id => {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+    );
+    const result = {
+      ...data,
+    };
+    return result;
+  } catch (error) {
+    console.error('Smth wrong with api ID fetch' + error);
+  }
+};
