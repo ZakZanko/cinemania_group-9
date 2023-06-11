e15313717a9d9cd97b5a4ac892935f5dc193625f
ZakZanko <zakzanko2112@gmail.com>
2023-06-11 22:39:14 +0300

    fixed

--- src/library.js
+++ src/library.js
@@ -3,7 +3,7 @@ import './js/header';
 import './js/hero';
 import './js/footer';
 import { createListMarkup } from './render';
-import { getMovieById2 } from './API/api';
+import { getMovieById2 } from './js/API/api';
 
 const librariesKey = 'b90b64a7e05f9e36894001e36eb3b3c7';
 let page = 1;
@@ -98,4 +98,3 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 };
-
