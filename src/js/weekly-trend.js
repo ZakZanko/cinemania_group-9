9da3f933e753ea5194271e069ddaf5b0bf3f1d34
ZakZanko <zakzanko2112@gmail.com>
2023-06-13 14:29:31 +0300

    fixed

--- src/js/weekly-trend.js
+++ src/js/weekly-trend.js
@@ -62,6 +62,12 @@
 // </div></li>`).join('')
 // }
 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const ENDPOINT = '/trending/movie/week';
+const API_KEY = 'b90b64a7e05f9e36894001e36eb3b3c7';
+// const IMG_W400 = /w400;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w400';
+
 const list = document.querySelector('.js-list');
 let page = 1;
 let options = {
@@ -168,54 +174,6 @@ async function createMarkup(arr) {
 getTrending()
   .then(async data => {
     list.insertAdjacentHTML('beforeend', await createMarkup(data.results));
-  })
-  .catch(err => console.log(err));
-
-const BASE_URL = 'https://api.themoviedb.org/3';
-const ENDPOINT = '/trending/movie/week';
-const API_KEY = 'b90b64a7e05f9e36894001e36eb3b3c7';
-// const IMG_W400 = `/w400`;
-const IMG_PATH = 'https://image.tmdb.org/t/p/w300';
-
-const list = document.querySelector('.js-list');
-let page = 1;
-let options = {
-  root: null,
-  rootMargin: '400px',
-  threshold: 0,
-};
-
-let observer = new IntersectionObserver(handlerPagination, options);
-
-function handlerPagination(entries, observer) {
-  // console.log(entries);
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      page += 1;
-      serviceMovie(page).then(data => {
-        list.insertAdjacentHTML('beforeend', createMarkup(data.results));
-        if (data.total_pages <= data.page) {
-          observer.unobserve(guard);
-        }
-      });
-    }
-  });
-}
-
-async function getTrending(page = 1) {
-  return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}`).then(
-    resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-
-      return resp.json();
-    }
-  );
-}
-getTrending()
-  .then(data => {
-    list.insertAdjacentHTML('beforeend', createMarkup(data.results));
   })
   .catch(err => console.log(err));
 
