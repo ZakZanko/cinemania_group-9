fd99e3e73b73fc4df43e791c431991322707f766
ZakZanko <zakzanko2112@gmail.com>
2023-06-15 15:58:13 +0300

    fixed

--- src/js/weekly-trend.js
+++ src/js/weekly-trend.js
@@ -62,79 +62,79 @@
 // </div></li>`).join('')
 // }
 
-const BASE_URL = 'https://api.themoviedb.org/3'; 
-const ENDPOINT = '/trending/movie/week'; 
-const API_KEY = 'b90b64a7e05f9e36894001e36eb3b3c7'; 
-// const IMG_W400 = /w400; 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w300'; 
- 
-const list = document.querySelector('.js-list'); 
-let page = 1; 
-let options = { 
-  root: null, 
-  rootMargin: '400px', 
-  threshold: 0, 
-}; 
- 
-let observer = new IntersectionObserver(handlerPagination, options); 
- 
-function handlerPagination(entries, observer) { 
-  // console.log(entries); 
-  entries.forEach((entry) => { 
-    if (entry.isIntersecting) { 
-      page += 1; 
-      serviceMovie(page).then((data) => { 
-        list.insertAdjacentHTML('beforeend', createMarkup(data.results)); 
-        if (data.total_pages <= data.page) { 
-          observer.unobserve(guard); 
-        } 
-      }); 
-    } 
-  }); 
-} 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const ENDPOINT = '/trending/movie/week';
+const API_KEY = 'b90b64a7e05f9e36894001e36eb3b3c7';
+// const IMG_W400 = /w400;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w300';
 
-async function getTrending(page = 1) { 
-  return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}`).then( 
-    (resp) => { 
-      if (!resp.ok) { 
-        throw new Error(resp.statusText); 
-      } 
- 
-      return resp.json(); 
-    } 
-  ); 
-} 
- 
-async function fetchGenres(movie) { 
-  const response = await fetch( 
-    'https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=' + 
-      API_KEY 
-  ); 
- 
-  const data = await response.json(); 
-  console.log(movie);
-  let genresArray = []; 
-  const genres = data.genres; 
- 
-  for (const genre of genres) { 
-    if (movie.genre_ids.includes(genre.id)) { 
-      genresArray.push(genre.name); 
-    } 
-  } 
-  return genresArray.slice(0, 2); 
-} 
+const list = document.querySelector('.js-list');
+let page = 1;
+let options = {
+  root: null,
+  rootMargin: '400px',
+  threshold: 0,
+};
+
+let observer = new IntersectionObserver(handlerPagination, options);
+
+function handlerPagination(entries, observer) {
+  // console.log(entries);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      serviceMovie(page).then(data => {
+        list.insertAdjacentHTML('beforeend', createMarkup(data.results));
+        if (data.total_pages <= data.page) {
+          observer.unobserve(guard);
+        }
+      });
+    }
+  });
+}
+
+async function getTrending(page = 1) {
+  return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}`).then(
+    resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+
+      return resp.json();
+    }
+  );
+}
+
+async function fetchGenres(movie) {
+  const response = await fetch(
+    'https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=' +
+      API_KEY
+  );
+
+  const data = await response.json();
+  let genresArray = [];
+  const genres = data.genres;
+
+  for (const genre of genres) {
+    if (movie.genre_ids.includes(genre.id)) {
+      genresArray.push(genre.name);
+    }
+  }
+  return genresArray.slice(0, 2);
+}
 //  --------------------------------------------------------------------------------------------------
 
-
-
-const createMarkup = async (arr) => {
+const createMarkup = async arr => {
   const genresPromises = arr.map(({ genre_ids }) => fetchGenres({ genre_ids }));
-  const genresArrays = (await Promise.all(genresPromises));
+  const genresArrays = await Promise.all(genresPromises);
 
   return arr
     .slice(0, 3)
     .map(
-      ({ original_title, poster_path, vote_average, id, release_date }, index) => {
+      (
+        { original_title, poster_path, vote_average, id, release_date },
+        index
+      ) => {
         const movieGenres = genresArrays[index];
         return `<li class='cards-list-item' data-id='${id}'>
           <img class='cards__list-img' src="${IMG_PATH}${poster_path}" alt="${original_title}" loading="lazy" 
@@ -143,7 +143,9 @@ const createMarkup = async (arr) => {
           <div class='weekly-trends__overlay'></div>
           <div class='cards__bloc-stars'>
             <h2 class='cards__list-title'>${original_title}</h2>
-            <div class='cards__list-text'>${movieGenres.join(', ')} | ${release_date.slice(0, 4)}</div>
+            <div class='cards__list-text'>${movieGenres.join(
+              ', '
+            )} | ${release_date.slice(0, 4)}</div>
           </div>
           <div class="weekly__rating">
             <div class="weekly__stars">${getStars(vote_average)}</div>
@@ -155,10 +157,9 @@ const createMarkup = async (arr) => {
 };
 
 function getStars(vote_average) {
- 
-// ----------------------------------------------------------
-const fullStar = '★';
-const emptyStar = '☆';
+  // ----------------------------------------------------------
+  const fullStar = '★';
+  const emptyStar = '☆';
   const roundedRating = Math.round(vote_average);
   let stars = '';
   for (let i = 0; i < 5; i++) {
@@ -190,11 +191,10 @@ async function displayMovieRating(rating) {
   ratingValueElement.textContent = `Rating: ${rating.toFixed(1)}`;
 }
 
-getTrending() 
-  .then(async (data) => { 
-    list.insertAdjacentHTML('beforeend', await createMarkup(data.results)); 
-  }) 
-  .catch((err) => console.log(err));
+getTrending()
+  .then(async data => {
+    list.insertAdjacentHTML('beforeend', await createMarkup(data.results));
+  })
+  .catch(err => console.log(err));
 
 // -------------------------------------
-
